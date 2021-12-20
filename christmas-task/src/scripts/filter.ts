f62e23c4b5a9226ebc2a0b669bede76584e154a4
Veniamin <venyasss@yandex.ru>
2021-12-20 14:59:27 +0300

    feat: add loading placeholder for toy img

--- christmas-task/src/scripts/filter.ts
+++ christmas-task/src/scripts/filter.ts
@@ -19,7 +19,9 @@ async function appendToysToHTML() {
     .map((toy) => `
     <div class="toy" data-toy-id="${toy.num}">
       <h4 class="toy__name">${toy.name}</h4>
-      <img class="toy__img" src="./assets/toys/${toy.num}.png">
+      <div class="toy__img-container">
+        <img class="toy__img toy__img_loading">
+      </div>
       <p class="toy__property">Amount: ${toy.count}</p>
       <p class="toy__property">Year bought: ${toy.year}</p>
       <p class="toy__property">Shape: ${toy.shape}</p>
@@ -29,6 +31,21 @@ async function appendToysToHTML() {
     </div>
     `)
     .join('');
+
+  toysContainer.querySelectorAll('.toy')
+    .forEach((toyElem) => {
+      const ind = ((<HTMLElement>toyElem).dataset.toyId ?? '0');
+      const imgContainerElem = toyElem.querySelector('.toy__img-container');
+      const imgElem = toyElem.querySelector('.toy__img');
+      if (!imgContainerElem || !imgElem) throw new Error('img elem not found');
+      const tempImg = document.createElement('img');
+      tempImg.src = `./assets/toys/${ind}.png`;
+      tempImg.addEventListener('load', () => {
+        (<HTMLImageElement>imgElem).classList.remove('toy__img_loading');
+        (<HTMLImageElement>imgElem).src = tempImg.src;
+        tempImg.remove();
+      });
+    });
 }
 
 document.querySelector('.toys__filters')?.addEventListener('change', () => {
