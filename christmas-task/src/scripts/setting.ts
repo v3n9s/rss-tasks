a476173a9e3e7da1356e58a5bd95698971eed30a
Veniamin <venyasss@yandex.ru>
2021-12-23 13:00:48 +0300

    feat: implement background setting

--- christmas-task/src/scripts/setting.ts
+++ christmas-task/src/scripts/setting.ts
@@ -12,6 +12,13 @@ const treeImgElem = <HTMLImageElement>document.querySelector('#tree-img');
 function changeTreeImg(url: string) {
   treeImgElem.src = url;
 }
+
+const backgroundImgElem = <HTMLImageElement>document.querySelector('#tree-bg-img');
+
+function changeBackgroundTreeImg(url: string) {
+  backgroundImgElem.src = url;
+}
+
 function addSetting({
   selector,
   imgUrl,
@@ -56,11 +63,33 @@ const trees: setting = {
 
 addSetting(trees);
 
+const background: setting = {
+  selector: '#setting-background',
+  imgUrl: './assets/svg/rss.svg',
+  items: [
+    './assets/bg/1.jpg',
+    './assets/bg/2.jpg',
+    './assets/bg/3.jpg',
+    './assets/bg/4.jpg',
+    './assets/bg/5.jpg',
+    './assets/bg/6.jpg',
+    './assets/bg/7.jpg',
+    './assets/bg/8.jpg',
+    './assets/bg/9.jpg',
+    './assets/bg/10.jpg',
+  ],
+  action: 'tree-bg-img',
+};
+
+addSetting(background);
+
 document.querySelector('.tree')?.addEventListener('click', (event) => {
   const target = <HTMLElement>(<HTMLElement>event.target).closest('[data-action]');
   if (target) {
     if (target.dataset.action === 'tree-img' && target.dataset.imgUrl) {
       changeTreeImg(target.dataset.imgUrl);
+    } else if (target.dataset.action === 'tree-bg-img' && target.dataset.imgUrl) {
+      changeBackgroundTreeImg(target.dataset.imgUrl);
     }
   }
 });
