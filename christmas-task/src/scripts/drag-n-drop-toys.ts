83c13712ad8af1e507f0212aee6191204fe06023
Veniamin <venyasss@yandex.ru>
2021-12-24 19:37:20 +0300

    feat: implement drag-n-drop functionality

--- christmas-task/src/scripts/drag-n-drop-toys.ts
+++ christmas-task/src/scripts/drag-n-drop-toys.ts
@@ -45,3 +45,85 @@ async function appendDraggableToys() {
 }
 
 appendDraggableToys();
+
+function renderToysAmount() {
+  draggableToysContainer.querySelectorAll('.draggable-toys__item').forEach((item) => {
+    const toysAmount = item.querySelectorAll('.draggable-toys__img').length;
+    (<HTMLElement>item.querySelector('.setting__amount')).innerText = `${toysAmount}`;
+  });
+}
+
+function preventScroll(event: TouchEvent) {
+  if (event.cancelable) event.preventDefault();
+}
+
+function draggingToy(event: PointerEvent) {
+  (<HTMLImageElement>event.target).style.top = `${event.clientY}px`;
+  (<HTMLImageElement>event.target).style.left = `${event.clientX}px`;
+}
+
+function dropToy(event: PointerEvent) {
+  const target = <HTMLImageElement>event.target;
+
+  target.style.pointerEvents = 'none';
+  const dropableElem = document.elementFromPoint(event.clientX, event.clientY)?.closest('[data-dropable]');
+  const isStrictlyInsideDropable = !!dropableElem && [
+    document.elementFromPoint(
+      event.clientX - (target.offsetWidth / 2),
+      event.clientY,
+    )?.closest('[data-dropable]'),
+    document.elementFromPoint(
+      event.clientX - (target.offsetWidth / 2),
+      event.clientY + target.offsetHeight,
+    )?.closest('[data-dropable]'),
+    document.elementFromPoint(
+      event.clientX + (target.offsetWidth / 2),
+      event.clientY + target.offsetHeight,
+    )?.closest('[data-dropable]'),
+    document.elementFromPoint(
+      event.clientX + (target.offsetWidth / 2),
+      event.clientY,
+    )?.closest('[data-dropable]'),
+  ].every((elem) => elem === dropableElem);
+  target.style.pointerEvents = 'all';
+
+  if (dropableElem && isStrictlyInsideDropable) {
+    const outerPads = dropableElem.getBoundingClientRect();
+    const pads = target.getBoundingClientRect();
+    target.style.top = `${((pads.top - outerPads.top) / (<HTMLElement>dropableElem).offsetHeight) * 100}%`;
+    target.style.left = `${((pads.left - outerPads.left) / (<HTMLElement>dropableElem).offsetWidth) * 100}%`;
+    target.classList.add('draggable-toys__img_dropped');
+    dropableElem.append(target);
+  } else {
+    target.classList.add('setting__img');
+    const { toyNum } = target.dataset;
+    if (!toyNum) throw new Error('Img doesn\'t have necessary attribute');
+    (<HTMLElement>document.querySelector(`.setting__item[data-toy-num="${toyNum}"]`)).append(target);
+  }
+  target.classList.remove('draggable-toys__img_dragging');
+  target.removeEventListener('touchmove', preventScroll);
+  target.removeEventListener('pointermove', draggingToy);
+  target.removeEventListener('pointerup', dropToy);
+  target.removeEventListener('pointercancel', dropToy);
+  renderToysAmount();
+}
+
+function dragToy(event: PointerEvent) {
+  const toyElem = <HTMLImageElement>(<HTMLElement>event.target).closest('[data-draggable]');
+  if (toyElem) {
+    document.body.append(toyElem);
+    toyElem.setPointerCapture(event.pointerId);
+    toyElem.classList.remove('setting__img');
+    toyElem.classList.remove('draggable-toys__img_dropped');
+    toyElem.classList.add('draggable-toys__img_dragging');
+    toyElem.style.top = `${event.clientY}px`;
+    toyElem.style.left = `${event.clientX}px`;
+    toyElem.addEventListener('touchmove', preventScroll);
+    toyElem.addEventListener('pointermove', draggingToy);
+    toyElem.addEventListener('pointerup', dropToy);
+    toyElem.addEventListener('pointercancel', dropToy);
+    renderToysAmount();
+  }
+}
+
+document.addEventListener('pointerdown', dragToy);
